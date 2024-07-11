ec8422adbc9ae5a831bab875cc377df726648b9d
Vidit <viditsn@gmail.com>
2024-07-11 16:45:28 +0530

    minor changes

--- backend/src/tutor/components/load_documents.py
+++ backend/src/tutor/components/load_documents.py
@@ -7,14 +7,12 @@ from pinecone import Pinecone, ServerlessSpec
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 from llama_index.core import StorageContext
-from IPython.display import Markdown, display
-from llama_parse import LlamaParse
 
 dotenv.load_dotenv()
 nest_asyncio.apply()
 
 
-class DocumentReader:
+class DocumentLoader:
     def __init__(self, folder_path):
         self.folder_path = folder_path
         self.LLAMA_API_KEY = os.getenv("LLAMA_API_KEY")
@@ -28,16 +26,18 @@ class DocumentReader:
         self.embed_model_name = "sentence-transformers/paraphrase-MiniLM-L6-v2"
 
     def parse_data(self):
-        
-        pdf_documents = SimpleDirectoryReader(os.path.join(self.folder_path,"pdf"),file_extractor=self.file_extractor).load_data()  
-        csv_documents = SimpleDirectoryReader(os.path.join(self.folder_path,"csv"),file_extractor=self.file_extractor).load_data()  
-        txt_documents = SimpleDirectoryReader(os.path.join(self.folder_path,"txt"),file_extractor=self.file_extractor).load_data()  
-        
-        return {
-            "parsed_pdf" : pdf_documents,
-            "parser_cvs" : csv_documents,
-            "parsed_txt" : txt_documents
-        }
+        try:
+            pdf_documents = SimpleDirectoryReader(os.path.join(self.folder_path,"pdf"),file_extractor=self.file_extractor).load_data()  
+            csv_documents = SimpleDirectoryReader(os.path.join(self.folder_path,"csv"),file_extractor=self.file_extractor).load_data()  
+            txt_documents = SimpleDirectoryReader(os.path.join(self.folder_path,"txt"),file_extractor=self.file_extractor).load_data()  
+            
+            return {
+                "parsed_pdf" : pdf_documents,
+                "parser_cvs" : csv_documents,
+                "parsed_txt" : txt_documents
+            }
+        except Exception as e:
+            return f"Error: {e}"
     
     def store_to_pinecone(
             self,
@@ -47,14 +47,23 @@ class DocumentReader:
             embedding_model,
             storage_type = "default"    # default or custom
         ):
-        pc = Pinecone(api_key=self.PINECONE_API_KEY)
-        pc_index_instance = pc.Index(index_name)
-        vector_store = PineconeVectorStore(pinecone_index=pc_index_instance,namespace=namespace)
-        storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        retrieval_index = VectorStoreIndex.from_documents(
-            data, 
-            storage_context=storage_context,
-            embed_model=embedding_model
-        )
+        
+        try:
+            pc = Pinecone(api_key=self.PINECONE_API_KEY)
+            pc_index_instance = pc.Index(index_name)
+            
+            if storage_type == "default":
+                vector_store = PineconeVectorStore(pinecone_index=pc_index_instance,namespace=namespace)
+                storage_context = StorageContext.from_defaults(vector_store=vector_store)
+                retrieval_index = VectorStoreIndex.from_documents(
+                    data, 
+                    storage_context=storage_context,
+                    embed_model=embedding_model
+                )
+            
+            return True,"Stored to database", retrieval_index
+        
+        except Exception as e:
+            return False, f"Error: {e}", None
 
 
